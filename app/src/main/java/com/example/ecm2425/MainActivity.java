02686552460356b1f9b70b19a74464f7ade29fb8
Jason <jdswift94@gmail.com>
2023-02-08 16:51:41 +0000

    create log button creates a log and adds to static Log ArrayList. Ready to be sent to new activity via explicit intent

--- app/src/main/java/com/example/ecm2425/MainActivity.java
+++ app/src/main/java/com/example/ecm2425/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.ecm2425;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTitle;
+    TextView mBody;
+    Button mCreateLogButton;
+    // imageView, uploadButton
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* wire widgets */
+        mTitle = findViewById(R.id.main_logTitle);
+        mBody = findViewById(R.id.main_logBody);
+        mCreateLogButton = findViewById(R.id.main_createLog_btn);
+
+        /* when createLogButton is pressed, create Log object with input data */
+        mCreateLogButton.setOnClickListener( v -> {
+            /** add -> if null functionality **/
+            Log newLog = new Log();
+            String title = mTitle.getText().toString();
+            String body = mBody.getText().toString();
+            newLog.setTitle(title);
+            newLog.setBody(body);
+            Log.allLogs.add(newLog);
+            Toast.makeText(this, " " + title + " " + body + " " + newLog.getID(), Toast.LENGTH_LONG).show();
+        });
     }
 }
